f52bd74af8609964a145ef1eb96fcf0cde13fcd6
angmu <anglimu@163.com>
2016-06-25 05:26:54 +0800

    initial

---
+++ MGZhiHu/MGZhiHu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGZhiHu
+//
+//  Created by 穆良 on 16/4/27.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGZhiHu/MGZhiHu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGZhiHu
+//
+//  Created by 穆良 on 16/4/27.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
